98c0bd5b8014009ae42800011b30389d629117d6
최예슬 <yeseul9231@github.com>
2021-11-30 16:52:05 +0900

    메인->달고나 버튼 연결

---
+++ app/src/main/java/com/example/teamproject4/dalognaActivity.java
@@ -0,0 +1,16 @@
+package com.example.teamproject4;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class dalognaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.dalgona_main);
+    }
+
+}
