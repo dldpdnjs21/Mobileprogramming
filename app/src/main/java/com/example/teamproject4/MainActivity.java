df75ef0a0f38d2155d5838121f56c2aab143f573
최예슬 <yeseul9231@github.com>
2021-12-02 18:41:19 +0900

    홈버튼 돌아가기, 난이도 선택 xml

--- app/src/main/java/com/example/teamproject4/MainActivity.java
+++ app/src/main/java/com/example/teamproject4/MainActivity.java
@@ -23,5 +23,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+
+
+
+
+
     }
 }
--- app/src/main/java/com/example/teamproject4/dalognaActivity.java
+++ app/src/main/java/com/example/teamproject4/dalognaActivity.java
@@ -1,6 +1,9 @@
 package com.example.teamproject4;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,4 +16,16 @@ public class dalognaActivity extends AppCompatActivity {
         setContentView(R.layout.dalgona_main);
     }
 
+
+    public void onClick(View view) {
+
+        switch (view.getId()){
+            case R.id.button1:
+                setContentView(R.layout.activity_main);
+                break;
+
+            case R.id.button2:
+                break;
+        }
+    }
 }
