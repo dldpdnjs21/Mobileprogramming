1631a23c83efc9a3fbb14c726d5df2da81210200
ssuzz1n <nosujin03@naver.com>
2021-12-06 03:46:44 +0900

    유리판게임 수정중

--- app/src/main/java/com/example/teamproject4/GButton.java
+++ app/src/main/java/com/example/teamproject4/GButton.java
@@ -9,6 +9,7 @@ import androidx.appcompat.widget.AppCompatButton;
 public class GButton extends AppCompatButton {
     int x, y;
     boolean tempered;
+    static public int pass=0; //통과했는지 카운트해주는 변수
 
     public GButton(Context context, int x, int y,boolean a) { //생성자
         super(context);
@@ -16,14 +17,51 @@ public class GButton extends AppCompatButton {
         this.y = y;
         this.tempered=a;
         setBackground(getResources().getDrawable(R.drawable.glassxml));
+
+
+
     }
     public boolean GClick() {
+        if(tempered==false){
+            setBackground(getResources().getDrawable(R.drawable.glass2xml));
+            for(int i=0;i<5;i++)
+                for(int j=0;j<2;j++)
+                    play2Activity.buttons[i][j].setClickable(false); // 모든 버튼 클릭 비활성화
+        }else{
+            pass++;
+            if(pass==1){
+                setBackground(getResources().getDrawable(R.drawable.dalgonaxml));
+                play2Activity.buttons[4][0].setClickable(false);
+                play2Activity.buttons[4][1].setClickable(false);
+                play2Activity.buttons[3][0].setClickable(true);
+                play2Activity.buttons[3][1].setClickable(true);
+
+            }
+            if(pass==2){
+                play2Activity.buttons[3][0].setClickable(false);
+                play2Activity.buttons[3][1].setClickable(false);
+                play2Activity.buttons[2][0].setClickable(true);
+                play2Activity.buttons[2][1].setClickable(true);
+            }
+            if(pass==3){
+                play2Activity.buttons[2][0].setClickable(false);
+                play2Activity.buttons[2][1].setClickable(false);
+                play2Activity.buttons[1][0].setClickable(true);
+                play2Activity.buttons[1][1].setClickable(true);
+            }
+            if(pass==4){
+                play2Activity.buttons[1][0].setClickable(false);
+                play2Activity.buttons[1][1].setClickable(false);
+                play2Activity.buttons[0][0].setClickable(true);
+                play2Activity.buttons[0][1].setClickable(true);
+                setBackground(getResources().getDrawable(R.drawable.dalgonaxml));
+            }
+        }
+
         if(!isClickable()){
             return false;
         } // 이미 한번 클릭됐으면 블락 못열음
         setClickable(false);  //한번 열면 클릭 못하게
-
-
 
         return true;
     }
--- app/src/main/java/com/example/teamproject4/play2Activity.java
+++ app/src/main/java/com/example/teamproject4/play2Activity.java
@@ -4,17 +4,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TableLayout;
 import android.widget.TableRow;
+import android.widget.TextView;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class play2Activity extends AppCompatActivity {
     static GButton[][] buttons = new GButton[5][2];
-
+    TextView textView;
+    int count=0;
     @Override
     protected void onCreate(@Nullable Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
         setContentView(R.layout.glass_game);
-
+        textView=findViewById(R.id.textView3);
         int i=0,j=0;
         Boolean bb[][] = new Boolean[9][9];
         for (i = 0; i < 9; i++) {
@@ -33,6 +36,7 @@ public class play2Activity extends AppCompatActivity {
             TableRow tableRow = new TableRow(this);
             table.addView(tableRow);
             for (j = 0; j < 2; j++) {
+                count++;
                 buttons[i][j] = new GButton(this, i, j,bb[i][j]);
                 TableRow.LayoutParams layoutParams =
                         new TableRow.LayoutParams(
@@ -41,19 +45,29 @@ public class play2Activity extends AppCompatActivity {
                                 1.0f);
                 buttons[i][j].setLayoutParams(layoutParams);
                 tableRow.addView(buttons[i][j]);
-                /*
-                for(int x=0;x<4;x++)
-                    for(int y=0;y<2;y++)
-                        play1Activity.buttons[i][j].setClickable(false); */
 
-                for(int x=0;x<4;x++)
-                    for(int y=0;y<2;y++)
-                        buttons[i][j].setClickable(false);
+                buttons[0][0].setClickable(false);
+                if(count==2)
+                    buttons[0][1].setClickable(false);
+                if(count==3)
+                    buttons[1][0].setClickable(false);
+                if(count==4)
+                    buttons[1][1].setClickable(false);
+                if(count==5)
+                    buttons[2][0].setClickable(false);
+                if(count==6)
+                    buttons[2][1].setClickable(false);
+                if(count==7)
+                    buttons[3][0].setClickable(false);
+                if(count==8)
+                    buttons[3][1].setClickable(false);
+
 
                 buttons[i][j].setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
                         ((GButton) view).GClick();
+                        textView.setText(String.valueOf(GButton.pass));
                     }
                 });
             }
