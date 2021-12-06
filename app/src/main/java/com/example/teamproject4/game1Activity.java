f4f0bf21fb38864a2d5819f5f9b3610fe52a928c
최예슬 <yeseul9231@github.com>
2021-12-07 02:32:59 +0900

    달고나 난이도 팝업 설정,연결

--- app/src/main/java/com/example/teamproject4/game1Activity.java
+++ app/src/main/java/com/example/teamproject4/game1Activity.java
@@ -4,18 +4,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class game1Activity extends AppCompatActivity {
 
+    TextView txtResult;
+
     @Override
     protected void onCreate(@Nullable Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
         setContentView(R.layout.dalgona_main);
 
-        Button imageButton = (Button) findViewById(R.id.button);
+        txtResult = (TextView)findViewById(R.id.button3);
+
+        Button imageButton = (Button) findViewById(R.id.button1);
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -24,15 +29,45 @@ public class game1Activity extends AppCompatActivity {
             }
         });
 
-        Button imageButton2 = (Button) findViewById(R.id.button2);
+        /*Button imageButton2 = (Button) findViewById(R.id.button2);
         imageButton2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent2 = new Intent(getApplicationContext(), play1Activity.class);
                 startActivity(intent2);
             }
+        });*/
+        Button imageButton3= (Button) findViewById(R.id.button3);
+        imageButton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), popupActivity.class);
+                startActivity(intent);
+            }
         });
     }
+
+    //버튼
+    public void mOnPopupClick(View v){
+        //데이터 담아서 팝업(액티비티) 호출
+        Intent intent = new Intent(this, popupActivity.class);
+        intent.putExtra("data", "Test Popup");
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                //데이터 받기
+                String result = data.getStringExtra("result");
+                txtResult.setText(result);
+            }
+        }
+    }
+
+
 
 
  /*   public void onClick(View view) {
